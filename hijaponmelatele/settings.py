e672d0529e3bd89254e4ccf6011993c0695e1b69
Mario Corchero <mcorcherojim@bloomberg.net>
2021-01-15 18:59:25 +0100

    Read static root from environment

--- hijaponmelatele/settings.py
+++ hijaponmelatele/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,6 +129,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+if "HPLTV_STATIC_ROOT" in os.environ:
+    STATIC_ROOT = os.environ["HPLTV_STATIC_ROOT"]
 
 
 DATABASES = {
